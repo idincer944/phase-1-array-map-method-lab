9538f8577810a4dcb707a33833a962e3374acc78
İsmail <idincer944@gmail.com>
2023-03-24 23:15:40 +0300

    Assginment completed

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const newTutorials = tutorials.map(function (tutorial) {
+    const arr = tutorial.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join(" ");
+  })
+  return newTutorials
 }
+
+titleCased();
+console.log(titleCased())
